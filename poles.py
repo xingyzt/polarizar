f5d67049e48dcc5b407637fdfcf42d5c5303eb7f
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2023-01-09 00:10:36 -0800

    Working ground truth

--- poles.py
+++ poles.py
@@ -5,9 +5,15 @@ import random
 import time
 
 # colors
-red = (0,0,255)
-green = (0,255,0)
-blue = (255,0,0)
+RED =   (0,0,255)
+GREEN = (0,255,0)
+YELLOW =    (0,255,255)
+BLUE =  (255,0,0)
+PINK =  (255,0,255)
+CYAN =  (255,255,0)
+WHITE = (255,255,255)
+
+
 
 def rand_color():
     return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
@@ -28,32 +34,35 @@ def seg_intersect(a1,a2, b1,b2) :
 vid = cv.VideoCapture(0)
 
 cv.namedWindow("warped", cv.WINDOW_NORMAL)
-cv.namedWindow("path", cv.WINDOW_NORMAL)
+#cv.namedWindow("path", cv.WINDOW_NORMAL)
 
-grid_size = 140
-true_pts = np.float32([
+GRID_SIZE = 140
+TRUE_PTS = np.float32([
              [-1,-2],          [+1,-2],
     [-2,-1], [-1,-1], [ 0,-1], [+1,-1], [+2,-1],
              [-1, 0],          [+1, 0],
     [-2,+1], [-1,+1], [ 0,+1], [+1,+1], [+2,+1],
              [-1,+2],          [+1,+2]
 ])
-start_pos = np.float32([+1,+2])
+START_POS = np.float32([+1.5,+3])
+PATH_SIZE = 700
 
 # _position 
-old_pos = start_pos
+old_pos = START_POS
 
-path_img = np.zeros((1000,1000,3), np.uint8)
+path_img = np.zeros((PATH_SIZE,PATH_SIZE,3), np.uint8)
 def path_pt(pt):
-    return np.int16(grid_size*pt + 500)
+    return np.int16(PATH_SIZE/8*pt + PATH_SIZE/2)
 
 # Image features
 matcher = cv.BFMatcher(cv.NORM_L2, crossCheck=True)
 
 while True:
-      
+
     # Read every frame
     ret, img = vid.read()
+
+    #old_pos = START_POS
 
     # Mask by hue-saturation-value
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -81,25 +90,28 @@ while True:
 
         # Find bounding convex hull
         hull = cv.convexHull(contour)
-        #cv.drawContours(img, [hull], -1, blue, 1)
+        #cv.drawContours(img, [hull], -1, BLUE, 1)
 
         # Find bounding rotated rectangle
         rect = cv.minAreaRect(hull)
         box = np.int0(cv.boxPoints(rect))
-        #cv.drawContours(img, [box], -1, green, 1)
+        #cv.drawContours(img, [box], -1, GREEN, 1)
 
         # Find centroid
-        M = cv.moments(contour)
-        center = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
+        moments = cv.moments(contour)
+        center = np.float32([
+            moments['m10']/moments['m00'], 
+            moments['m01']/moments['m00']
+        ])
 
         # Fit line segment and find base of pole
         [[vx], [vy], [x], [y]] = cv.fitLine(hull, cv.DIST_L2, 0, 0.01, 0.01)
         [left, right] = sorted(box, key = lambda x:x[1])[2:]
         base = np.float32(seg_intersect(
-            np.array([x,y]), 
-            np.array([x+vx,y+vy]),
-            np.array(left), 
-            np.array(right))
+            np.float32([x,y]), 
+            np.float32([x+vx,y+vy]),
+            np.float32(left), 
+            np.float32(right))
         )
 
         # Perspective project these features
@@ -109,61 +121,65 @@ while True:
             ]), project_mat)[0]
         )
 
-        rel_pos = warped_base/grid_size
-        new_pt = old_pos + np.array([rel_pos[0], -rel_pos[1]])
+        rel_pos = np.float32([X/2-warped_base[0], Yf-warped_base[1]])/GRID_SIZE
+        new_pt = old_pos - rel_pos
         new_pts.append(new_pt)
 
-        cv.line(img, np.int16(base), np.int16(center), blue, 1)
-        cv.line(warped_img, np.int16(warped_base), np.int16(warped_center), blue, 1)
+        cv.line(img, np.int16(base), np.int16(center), BLUE, 1)
+        cv.line(warped_img, np.int16(warped_base), np.int16(warped_center), BLUE, 1)
 
-        cv.line(img, np.int16(left), np.int16(right), blue, 1)
-        cv.line(warped_img, np.int16(warped_left), np.int16(warped_right), blue, 1)
+        cv.line(img, np.int16(left), np.int16(right), BLUE, 1)
+        cv.line(warped_img, np.int16(warped_left), np.int16(warped_right), BLUE, 1)
+
+    new_pts = np.float32(new_pts)
 
     if len(new_pts) > 1:
         # Find similarities 
-        matches = matcher.match(true_pts, new_pts)
+        matches = matcher.match(TRUE_PTS, new_pts)
         matches = sorted(matches, key = lambda x:x.distance)
+        matches = [ x for x in matches if x.distance < 0.5 ]
 
-        matched_true_pts = []
-        matched_new_pts = []
+        matched_true_pts = np.float32([TRUE_PTS[match.queryIdx] for match in matches])
+        matched_new_pts = np.float32([new_pts[match.trainIdx] for match in matches])
 
-        for match in matches:
-
-            true_pt = true_pts[match.queryIdx]
-            new_pt = new_pts[match.trainIdx]
-
-            delta = np.subtract(new_pt, true_pt)
-
-            matched_true_pts.append(true_pt)
-            matched_new_pts.append(new_pt)
-
-            cv.line(path_img, path_pt(true_pt), path_pt(new_pt), red, 1)
+        print(len(matched_true_pts))
 
         if len(matched_new_pts) > 1:
 
             transformation, inliers = cv.estimateAffinePartial2D(
-                    np.array([matched_true_pts]),
-                    np.array([matched_new_pts])
+                    np.array([matched_new_pts]),
+                    np.array([matched_true_pts])
                 )
+            transformation[1][0] = 0
+            transformation[0][1] = 0
             new_pos = transformation.dot([old_pos[0], old_pos[1], 1])
+            print(new_pos)
 
             #fade path img
-            path_img = (path_img * 0.99).astype("uint8")
             cv.line(path_img, path_pt(old_pos), path_pt(new_pos), rand_color(), 2)
-            print(new_pos)
             old_pos = new_pos
 
     #cv.imshow("img", img)
-    cv.rectangle(warped_img, (236, 715), (236+grid_size, 715-grid_size), red)
+
+    path_img = (path_img * 0.8).astype("uint8")
+    cv.drawMarker(path_img, path_pt(START_POS), GREEN, cv.MARKER_DIAMOND, 25, 2)
+    cv.drawMarker(path_img, path_pt(old_pos), WHITE, cv.MARKER_CROSS, 20, 2)
+    for pt in new_pts:
+        cv.circle(path_img, path_pt(pt), 10, RED, 1)
+    for pt in matched_new_pts:
+        cv.circle(path_img, path_pt(pt), 10, PINK, 3)
+    for pt in TRUE_PTS:
+        cv.circle(path_img, path_pt(pt), 5, BLUE, 1)
+    for pt in matched_true_pts:
+        cv.circle(path_img, path_pt(pt), 5, BLUE, cv.FILLED)
+      
+    cv.rectangle(warped_img, (236, 715), (236+GRID_SIZE, 715-GRID_SIZE), RED)
     cv.imshow("warped", warped_img)
     cv.imshow("path", path_img)
 
-
-    ##time.sleep(0.5)
-      
     # the 'q' button is set as the
     # quitting button you may use any
-    # desired button of your choice
+    # desiRED button of your choice
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
   
